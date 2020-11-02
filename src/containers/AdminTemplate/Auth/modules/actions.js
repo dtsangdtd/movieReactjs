61d12c1862228d1768bddb61ce8149856a11f20c
dtsangnttq <sadwindsang@gmail.com>
2020-11-02 23:26:10 +0700

    update funtionc login user

--- src/containers/AdminTemplate/Auth/modules/actions.js
+++ src/containers/AdminTemplate/Auth/modules/actions.js
@@ -4,7 +4,24 @@ import {
   AUTH_PAGE_FAILED,
 } from './constants';
 import Axios from 'axios';
-export const actLoginAPI = () => {};
+export const actLoginAPI = (user) => {
+  return (dispatch) => {
+    dispatch(actLoginRequest());
+    // console.log(user);
+    Axios({
+      url: 'https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap',
+      method: 'POST',
+      data: user,
+    })
+      .then((result) => {
+        dispatch(actLoginSuccess(result));
+        console.log(result);
+      })
+      .catch((err) => {
+        dispatch(actLoginFailed(err));
+      });
+  };
+};
 
 const actLoginRequest = () => {
   return {
